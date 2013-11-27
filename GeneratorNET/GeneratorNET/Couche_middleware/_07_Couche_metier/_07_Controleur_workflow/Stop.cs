b7a2bd2d0cef972858d13dc6a249d036e1697cdc
Zeld1 <tony.fellet@gmail.com>
2013-11-27 11:58:30 +0100

    Ajout de la fonction stop et des variables globales

---
+++ GeneratorNET/GeneratorNET/Couche_middleware/_07_Couche_metier/_07_Controleur_workflow/Stop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Couche_middleware._07_Couche_metier._08_Composant_metier;
+
+namespace Couche_middleware._07_Couche_metier._07_Controleur_workflow
+{
+	public class Stop
+	{
+		public void Execute()
+		{
+			GlobalVariables.clientStop = true;
+		}
+	}
+}
